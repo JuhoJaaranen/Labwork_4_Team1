71f73d66660a9664abecacdf39c4685ab380d085
JuhoJaaranen <juho.jaaranen@gmail.com>
2025-10-22 19:54:40 +0300

    muutin jotain

--- To-do-list/src/App.tsx
+++ To-do-list/src/App.tsx
@@ -19,6 +19,13 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+
+
+
+
+
+
+
 /**
  * Ionic Dark Mode
  * -----------------------------------------------------
